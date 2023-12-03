8f63ab60479ed2011192522682724e9449c2ac08
KTH <trends.kts@gmail.com>
2023-12-03 12:22:21 +0900

    https 설정

---
+++ src/main/java/com/example/springboot/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.example.springboot.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        http.formLogin().disable();
+
+        http
+                .requiresChannel(channel -> channel.anyRequest().requiresSecure())
+                .authorizeRequests(authorize -> authorize.anyRequest().permitAll());
+
+        return http.build();
+    }
+}
